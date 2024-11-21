e9511f022cb61b63fdeccaf4600fdf35992ef178
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-11-21 18:30:24 +0300

    init

---
+++ src/main/java/me/jazzy/webtoontracker/WebtoonTrackerApplication.java
@@ -0,0 +1,13 @@
+package me.jazzy.webtoontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtoonTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebtoonTrackerApplication.class, args);
+    }
+
+}
